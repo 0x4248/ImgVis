b0a38f9ffe0e3d400c1ed25f9ca64dc69f345e96
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-09-03 10:14:13 +0100

    Add setup.py
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+# ImgVis
+# Visualize images with graphs
+# Github: https://www.github.com/lewisevans2007/ImgVis
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+from setuptools import setup
+
+setup(
+    name="ImgVis",
+    version="0.1",
+    packages=["ImgVis"],
+    install_requires=[
+        "numpy",
+        "Pillow",
+        "matplotlib"
+    ],
+)
