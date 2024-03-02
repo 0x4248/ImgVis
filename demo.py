b6b48fc18fd0939009510d8a3805d3123df05a16
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-03-02 15:22:49 +0000

    Format demo.py

--- demo.py
+++ demo.py
@@ -7,17 +7,18 @@
 # This demo file tests the ImgVis module and shows how to use it
 # This file is not required for the module to work
 
-
-
 import ImgVis
 
 demo_file = "test_img.jpeg"
 
+# 3D graphs
 ImgVis.colour_3d(demo_file, compression=50)
 ImgVis.brightness_3d(demo_file, compression=50)
 ImgVis.red_3d(demo_file, compression=50)
 ImgVis.green_3d(demo_file, compression=50)
 ImgVis.blue_3d(demo_file, compression=50)
+
+# 2D graphs
 ImgVis.red_line_2d(demo_file)
 ImgVis.green_line_2d(demo_file)
 ImgVis.blue_line_2d(demo_file)
